a5f55bbf773c363493932e2bbf27094a181edd13
Paulo Fernandes <phfernandespereira@gmail.com>
2023-05-26 18:25:38 -0300

    feat: adiciona testes de usuairos

--- spec/models/usuario_spec.rb
+++ spec/models/usuario_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe Usuario, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should_not be_valid }
+
+  context "criacao do usuario" do
+    it "email e senha são obrigatórios" do
+      usuario = Usuario.new email: Faker::Internet.email, password: Faker::Internet.password
+      expect(usuario).to be_valid
+    end
+
+    it "valida formato de email" do
+      usuario = Usuario.new email: Faker::Name.name, password: Faker::Internet.password
+      expect(usuario).to_not be_valid
+    end
+
+    it "valida presença de senha" do
+      usuario = Usuario.new email: Faker::Internet.email
+      expect(usuario).to_not be_valid
+    end
+  end
 end

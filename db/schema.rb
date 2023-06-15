1ff426b120f2cb055f7ccf39abbfeaecca385ecd
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-06-15 02:18:40 -0300

    feat: adiciona coluna de nome aos usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_010226) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_15_040508) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -92,6 +92,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_010226) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin"
+    t.string "nome"
     t.index ["email"], name: "index_usuarios_on_email", unique: true
     t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true
   end

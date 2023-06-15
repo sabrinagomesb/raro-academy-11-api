554b89020062869c6bde1020c192895e76832fc4
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-06-15 02:19:28 -0300

    test: adiciona testes referente ao ranking

--- spec/factories/usuario.rb
+++ spec/factories/usuario.rb
@@ -2,6 +2,7 @@ require 'factory_bot'
 
 FactoryBot.define do
   factory :usuario do
+    nome { Faker::Name::name }
     email { Faker::Internet::email }
     password { Faker::Internet::password }
   end

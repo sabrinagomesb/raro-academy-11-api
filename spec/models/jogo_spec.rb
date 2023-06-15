162c98ca0e8d6ec6172606171ac1a5d7c14b2a14
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-06-15 02:24:03 -0300

    test: adiciona testes referente ao ranking

--- spec/models/jogo_spec.rb
+++ spec/models/jogo_spec.rb
@@ -6,5 +6,41 @@ RSpec.describe Jogo, type: :model do
   it { should belong_to(:mandante).class_name('Equipe') }
   it { should belong_to(:visitante).class_name('Equipe') }
   it { should belong_to :rodada }
-  it { should have_many :palpites } 
+  it { should have_many :palpites }
+
+  context '#empate?' do
+    it 'deve retornar true se o jogo terminou empatado' do
+      jogo = Jogo.new gols_mandante: 1, gols_visitante: 1
+      expect(jogo.empate?).to be_truthy
+    end
+
+    it 'deve retornar false se o jogo não terminou empatado' do
+      jogo = Jogo.new gols_mandante: 1, gols_visitante: 0
+      expect(jogo.empate?).to be_falsey
+    end
+  end
+
+  context '#vitoria_mandante?' do
+    it 'deve retornar true se o mandante venceu' do
+      jogo = Jogo.new gols_mandante: 1, gols_visitante: 0
+      expect(jogo.vitoria_mandante?).to be_truthy
+    end
+
+    it 'deve retornar false se o mandante não venceu' do
+      jogo = Jogo.new gols_mandante: 0, gols_visitante: 1
+      expect(jogo.vitoria_mandante?).to be_falsey
+    end
+  end
+
+  context '#vitoria_visitante?' do
+    it 'deve retornar true se o visitante venceu' do
+      jogo = Jogo.new gols_mandante: 0, gols_visitante: 1
+      expect(jogo.vitoria_visitante?).to be_truthy
+    end
+
+    it 'deve retornar false se o visitante não venceu' do
+      jogo = Jogo.new gols_mandante: 1, gols_visitante: 0
+      expect(jogo.vitoria_visitante?).to be_falsey
+    end
+  end
 end
